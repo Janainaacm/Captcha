80dcbe46a00da3a067de0a49020deb4d5ba00084
Janainaacm <Janaina.maneiro@gmail.com>
2025-04-22 10:37:28 +0200

    Added: CaptchaConfig, Lombok dep

---
+++ be/src/main/java/com/example/be/config/CaptchaConfig.java
@@ -0,0 +1,70 @@
+package com.example.be.config;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.awt.*;
+
+@Getter
+@Setter
+public final class CaptchaConfig {
+    private int width = 120;
+
+    private int height = 40;
+
+    private int length = 5;
+
+    private int noise = 15;
+
+    private boolean dark = false;
+
+    private Color[] lightPalette = {Color.BLACK, Color.DARK_GRAY, Color.BLUE, Color.GREEN, Color.RED};
+
+    private Color[] darkPalette = {Color.WHITE, Color.LIGHT_GRAY, Color.CYAN, Color.MAGENTA, Color.ORANGE};
+
+    private Color darkBackgroundColor = new Color(30, 30, 30);
+
+    private Color lightBackgroundColor = Color.WHITE;
+
+    private int[] fontStyles = {Font.PLAIN, Font.BOLD, Font.ITALIC, Font.BOLD + Font.ITALIC};
+
+    private String[] fonts = {};
+
+    public CaptchaConfig() {
+    }
+
+    public CaptchaConfig(int width, int height, int length, int noise, boolean dark, Color[] lightPalette, Color[] darkPalette,
+                         Color lightBackgroundColor, Color darkBackgroundColor, int[] fontStyles, String[] fonts) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
+        this.noise = noise;
+        this.dark = dark;
+        this.lightPalette = lightPalette;
+        this.darkPalette = darkPalette;
+        this.lightBackgroundColor = lightBackgroundColor;
+        this.darkBackgroundColor = darkBackgroundColor;
+        this.fontStyles = fontStyles;
+        this.fonts = fonts;
+    }
+
+    public CaptchaConfig(int width, int height, int length, int noise, boolean dark, Color lightBackgroundColor,
+                         Color darkBackgroundColor) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
+        this.noise = noise;
+        this.lightBackgroundColor = lightBackgroundColor;
+        this.darkBackgroundColor = darkBackgroundColor;
+        this.dark = dark;
+    }
+
+    public CaptchaConfig(int width, int height, int length, int noise, boolean dark) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
+        this.noise = noise;
+        this.dark = dark;
+    }
+}
